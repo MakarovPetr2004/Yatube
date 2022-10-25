6bf74b320461938428475ab36e4174ea2f2d5f77
Макаров Пётр <mr.ru.20042004@gmail.com>
2022-10-25 22:06:07 +0300

    Everything but last task was done

--- yatube/groups/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def group_posts(request, slug):
-    return HttpResponse(f'Страница группы {slug}')
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date'[:10])
+    context = {
+        'posts': posts
+    }
+    return render(request, template, context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    template = 'groups/index.html'
+    return render(request, template, context)
